95da7f251a8a6d06abf0d6d57befcd7c4e0fdfb4
berger815 <129002430+berger815@users.noreply.github.com>
2026-09-06 00:27:18 -0400

    Bump VEYOS shell cache to v0.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='veyos-shell-v03';
+const CACHE='veyos-shell-v04';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./trips/asia-2026.sample.json','./trips/antigua.sample.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
